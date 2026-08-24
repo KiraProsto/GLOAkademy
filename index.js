357746b65509fa2a0746deefa4e3a73c1b53067e
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-08-24 19:49:15 +0300

    add data types and operators

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const title = 'Lesson1';
-const screens = 1;
+const title = 'Lesson2';
+const screens = 'Простые, Сложные, Интерактивные';
 const screenPrice = 1000;
-let rollback = 0.25;
-let fullPrice = 850;
+let rollback = 25;
+let fullPrice = 123456789000000000000000;
 let adaptive = true;
 
 alert('Hello, world!');
 console.log('this is console');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + 'долларов');
+console.log('Стоимость разработки сайта ' + fullPrice + ' долларов');
+
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback / 100));
